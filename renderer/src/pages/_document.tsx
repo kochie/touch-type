d7a4024b68116f9f054331ab7aa1e1992b32a0f1
Robert Koch <robert@kochie.io>
2022-08-11 11:41:27 +0800

    adds styles for windows

--- renderer/src/pages/_document.tsx
+++ renderer/src/pages/_document.tsx
@@ -1,6 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 // import "../assets/RobotoMono-Regular.ttf";
 
+const os = require("os");
+
+const isMac = os.platform() === "darwin";
+const isWindows = os.platform() === "win32";
+const isLinux = os.platform() === "linux";
+
 class MyDocument extends Document {
   render() {
     return (
@@ -17,7 +23,13 @@ class MyDocument extends Document {
             rel="stylesheet"
           /> */}
         </Head>
-        <body>
+        <body
+          className={
+            isWindows
+              ? "dark:text-white text-black dark:bg-zinc-800 bg-zinc-300"
+              : ""
+          }
+        >
           <Main />
           <NextScript />
         </body>
