f7e5f51bb94538d5dea4d36dd2632bfc968d04f0
Robert Koch <robert@kochie.io>
2022-12-02 09:22:01 +1100

    updated logo

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -10,6 +10,7 @@ const sentryWebpackPluginOptions = {
   // recommended:
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
+  hideSourceMaps: false,
 
   silent: true, // Suppresses all logs
   // For all available options, see:
