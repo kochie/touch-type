07b34782a706c286fd5210df467aec52cfcac0ac
Robert Koch <robert@kochie.io>
2023-05-30 18:45:47 +1000

    added new multi-language keyboards

--- renderer/src/components/Tracker/index.tsx
+++ renderer/src/components/Tracker/index.tsx
@@ -14,8 +14,6 @@ import sampleSize from "lodash.samplesize";
 import { KeyboardLayoutNames, useSettings } from "@/lib/settings_hook";
 import { useWords } from "@/lib/word-provider";
 import { MACOS_US_COLEMAK, MACOS_US_DVORAK, MACOS_US_QWERTY, lookupKeyboard } from "@/keyboards";
-import { useSettings } from "@/lib/settings_hook";
-import { lookupKeyboard } from "@/keyboards";
 
 // import wordBlob from "@/assets/words.txt";
 
