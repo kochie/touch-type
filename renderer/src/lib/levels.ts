4c982a9e560adef959f98aa816f080169a2a5a51
Robert Koch <robert@kochie.io>
2024-08-06 01:28:24 +1000

    Add new regex patterns for levels 4 and 5

--- renderer/src/lib/levels.ts
+++ renderer/src/lib/levels.ts
@@ -1,16 +1,31 @@
-const LEVEL_1_QWERTY = /^[asdfghjkl]+$/u;
-const LEVEL_2_QWERTY = /^[asdfghjklzxcvbnm]+$/u;
-const LEVEL_3_QWERTY = /^[asdfghjklzxcvbnmqwertyuiop]+$/u;
+export const LEVEL_1_QWERTY = /^[adsfjkl]+$/u;
+export const LEVEL_2_QWERTY = /^[asdfghjkl]+$/u;
+export const LEVEL_3_QWERTY = /^[asdfjklzxcvnm]+$/u;
+export const LEVEL_4_QWERTY = /^[qwerasdfzxcvuiopjklnm]+$/u;
+export const LEVEL_5_QWERTY = /^[qwertyuiopasdfghjklzxcvbnm]+$/u;
 
-const LEVEL_1_QWERTY_MI = /^[asdfghjklā]+$/u;
-const LEVEL_2_QWERTY_MI = /^[asdfghjklzxcvbnmā]+$/u;
-const LEVEL_3_QWERTY_MI = /^[asdfghjklzxcvbnmqwertyuiopāēīōū]+$/u;
+export const LEVEL_1_DVORAK = /^[aoeuhtns]+$/u;
+export const LEVEL_2_DVORAK = /^[aoeuidhtns]+$/u;
+export const LEVEL_3_DVORAK = /^[aoeuhtnsqjkbmwvz]+$/u;
+export const LEVEL_4_DVORAK = /^[aoeuhtnsqjkbmwvzdix]+$/u;
+export const LEVEL_5_DVORAK = /^[aoeuhtnsqjkbmwvzdixpyfgcrl]+$/u;
+ 
+export const LEVEL_1_COLEMAK = /^[arstneio]+$/u;
+export const LEVEL_2_COLEMAK = /^[arstneiodh]+$/u;
+export const LEVEL_3_COLEMAK = /^[arstneiozxcvkm]+$/u;
+export const LEVEL_4_COLEMAK = /^[arstneiozxcvkmqwfpluy]+$/u;
+export const LEVEL_5_COLEMAK = /^[qwertyuiopasdfghjklzxcvbnm]+$/u;
+ 
+export const LEVEL_1_AZERTY = /^[]+$/u;
+export const LEVEL_2_AZERTY = /^[]+$/u;
+export const LEVEL_3_AZERTY = /^[]+$/u;
+export const LEVEL_4_AZERTY = /^[]+$/u;
+export const LEVEL_5_AZERTY = /^[]+$/u;
 
-export {
-  LEVEL_1_QWERTY,
-  LEVEL_2_QWERTY,
-  LEVEL_3_QWERTY,
-  LEVEL_1_QWERTY_MI,
-  LEVEL_2_QWERTY_MI,
-  LEVEL_3_QWERTY_MI,
-};
+ 
+export const LEVEL_1_QWERTY_MI = /^[asdfghjklā]+$/u;
+export const LEVEL_2_QWERTY_MI = /^[asdfghjklzxcvbnmā]+$/u;
+export const LEVEL_3_QWERTY_MI = /^[asdfghjklzxcvbnmqwertyuiopāēīōū]+$/u;
+export const LEVEL_4_QWERTY_MI = /^[asdfghjklzxcvbnmqwertyuiopāēīōūhjklā]+$/u;
+export const LEVEL_5_QWERTY_MI = /^[asdfghjklzxcvbnmqwertyuiopāēīōūhjklā]+$/u;
+
--- renderer/src/lib/settings_hook.tsx
+++ renderer/src/lib/settings_hook.tsx
@@ -28,6 +28,8 @@ export enum Levels {
   LEVEL_1 = "1",
   LEVEL_2 = "2",
   LEVEL_3 = "3",
+  LEVEL_4 = "4",
+  LEVEL_5 = "5",
 }
 
 export enum ColorScheme {
